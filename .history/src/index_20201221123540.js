198ec07e5ef079f76f7c7d0d64d6b01f3d837db9
Oluwafemi <akinsiku13@gmail.com>
2020-12-21 12:41:35 +0100

    update cable url

--- .history/src/index_20201221123540.js
+++ .history/src/index_20201221123540.js
@@ -9,7 +9,7 @@ import actionCable from 'actioncable';
 const CableApp = {}
 // CableApp.cable = actionCable.createConsumer("ws://localhost:3001/cable");
 CableApp.cable = actionCable.createConsumer(
-  "wss://peeps-platform.herokuapp.com/cable"
+  "wss://peeps-frontend.vercel.app/cable"
 );
 
 
